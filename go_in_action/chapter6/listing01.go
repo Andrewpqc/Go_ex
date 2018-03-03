37f1ec9952811e06eebd1bd67b0b64d84a7ca0ef
Andrewpqc <m13636038496@163.com>
2018-03-03 21:31:49 +0800

    add notes for go in action

---
+++ go_in_action/chapter6/listing01.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	// "runtime"
+	"sync"
+)
+
+func main(){
+	// runtime.GOMAXPROCS(1)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	fmt.Println("start goroutines...")
+
+	go func(){
+		defer wg.Done()
+		for count:=0;count<3;count++{
+			for char:='A';char<'A'+26;char++ {
+				fmt.Printf("%c\n",char)
+			}
+		}
+	}()
+
+	go func(){
+		defer wg.Done()
+		for count:=0;count<3;count++{
+			for char:='a';char<'a'+26;char++{
+				fmt.Printf("%c\n",char )
+			}
+		}
+	}()
+	fmt.Println("waiting to finish...")
+	wg.Wait()
+}
